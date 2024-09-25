6952b4267cb21294b301c0eb638c3ce0dfad11ca
ggodderis <goueg@orange.fr>
2024-09-25 14:48:01 +0200

    mercredi 25 sept

---
+++ class/back_rest_route_helloasso.php
@@ -0,0 +1,13 @@
+<?php
+
+class back_rest_route_helloasso {
+    
+
+    public function __construct() {
+        //add_action('rest_api_init', array(&$this,'create_rest_route') );
+    }
+
+}
+
+
+?>
