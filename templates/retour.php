6df4c8303e5913ca5fec1a391f2c3b62a73bf91a
Gregory Godderis <g.godderis@spartoo.com>
2024-10-22 16:54:08 +0200

    Mardi 22 oct v2

--- templates/retour.php
+++ templates/retour.php
@@ -172,21 +172,66 @@ class retourHello {
     private function createAndUpdateAdherent( $create, $statut ){
         /**
          * Si le champs create de la table wp_client
-         * est en "attente" on fait le travail si non on fait rien..
+         * est en "attente" et que le status est sur 2 = validé
+         * on fait le travail si non on fait rien..
          */
         $metadata = $this->datas['metadata'];
 
         if( $create == "attente" && intval($statut) == 2 ){
 
-            echo '<pre>';
-            print_r($metadata['payer']);
-            echo '</pre>';
+            $options    = $metadata['options_ffme'];
+            $payer      = $metadata['payer'];
+            $options_liste = [];
 
-            update_user_meta( $metadata['payer']['id'], 'gda_type_cotisation', 'A');
-            update_user_meta( $metadata['payer']['id'], 'gda_licence', 'FFR_FRA');
-            update_user_meta( $metadata['payer']['id'], 'gda_licence_speciale', ['BASE+']);
-            update_user_meta( $metadata['payer']['id'], 'gda_lieu_naissance', 'Croix');
-            update_user_meta( $metadata['payer']['id'], 'gda_saison', '2025');
+            /**
+             * On liste les options ffme si y'en a...
+             */
+                if( isset( $options ) &&  !empty( $options ) ){
+                    foreach( $options as $key => $value ){
+                        if( $value['checked'] ){
+                            array_push( $options_liste, $value['name'] );
+                        }
+                    }
+                }
+
+            /**
+             * Si c'est déjà un adhérent
+             * update des ses infos et mail de facture
+             */
+                if( isset( $payer['id'] ) && !empty( $payer['id'] ) ){
+                    // update_user_meta( $metadata['payer']['id'], 'gda_inscript_escalade', intval($metadata['mur]) > 0 ? 'oui' : '' );
+                    // update_user_meta( $metadata['payer']['id'], 'gda_type_cotisation', $metadata['cotisation'] );
+                    // update_user_meta( $metadata['payer']['id'], 'gda_licence', $metadata['type_licence'] );
+                    // update_user_meta( $metadata['payer']['id'], 'gda_licence_speciale', $options_liste );
+                    // update_user_meta( $metadata['payer']['id'], 'gda_saison', self::annee_saison_en_cours());
+                }
+            /**/
+                $to = 'goueg4@gmail.com';
+                $subject = "Adhésion au Club Grimpeurs des Alpes";
+
+                // ob_start();
+                // include(get_stylesheet_directory() . '/email-template.php');//Template File Path
+                // $body = ob_get_contents();
+                // ob_end_clean();
+                $body = '<h1>Adhésion mise à jour...</h1>';
+
+                $headers = array(
+                        'Content-Type: text/html; charset=UTF-8',
+                        'Reply-To: info@club-montagne.net',
+                        'From: Grimpeurs des Alpes <info@club-montagne.net>');
+
+                wp_mail( $to, $subject, $body, $headers );
+            
+
+            /**
+             * Si c'est un nouvel adhérent
+             * création de l'adhérent
+             * mail de facture et mail mot de passe
+             */
+
+            echo '<pre>';
+            print_r($metadata);
+            echo '</pre>';
             
             $password = wp_generate_password(6, false);
 
@@ -213,155 +258,34 @@ class retourHello {
         }
     }
 
+
+    /**
+    * Retourne l'année pour la zone compte et l'affichage de la saison en cours
+    * à savoir que l'année d'adhésion commence le 1er septembre
+    */
+    private function annee_saison_en_cours():string {
+
+        $date = new DateTime('now',new DateTimeZone('Europe/Paris'));
+        $mois = intval ( (clone $date)->format('m') );
+
+        if( $mois >= 9 && $mois <= 12 ){
+
+        $annee = (clone $date)->modify('next year')->format('Y');
+
+        }else{
+        $annee = (clone $date)->format('Y');
+        }
+
+        return $annee;
+    }
+
 }
 
 
 $initRetour = new retourHello();
 
-// get_footer();
-
 
 exit();
-
-
-// if( isset($_GET['checkoutIntentId']) && !empty($_GET['checkoutIntentId']) ){
-
-//     $table_name = 'wp_clients';
-//     $checkoutIntentId = $_GET['checkoutIntentId'];
-
-//     /**
-//      * Demande de token
-//      * à modifier pour la prod !!
-//      */
-//     $url = 'https://api.helloasso-sandbox.com/oauth2/token';
-
-//     $payload =  array( 
-//         "client_id" => "a0fead5a1ba4404eacf4b9de755fb3b9",
-//         "client_secret" => "NOfvEhtPEa4CBhrom8Q6nkBIwcZOapxr",
-//         "grant_type" => "client_credentials"
-//     );
-
-
-//     $curl = curl_init($url);
-//             curl_setopt($curl, CURLOPT_POST, true );
-//             curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded'] );
-//             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//             curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($payload) );
-
-//     $data = curl_exec($curl);
-//     $data = json_decode($data);
-//     $access_token = $data->access_token;
-
-//     /**
-//      * Demande d'infos grace au token
-//      * à modifier pour la prod !!
-//      */  
-//     $url2 = "https://api.helloasso-sandbox.com/v5/organizations/goueg/checkout-intents/$checkoutIntentId";
-//     $curl2 = curl_init($url2);
-            
-//             curl_setopt($curl2, CURLOPT_HTTPHEADER, ["Authorization: Bearer $access_token"] );
-//             curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
-
-//     $retour_hello = curl_exec($curl2);
-//     $retour_hello = json_decode($retour_hello,true);
-
-// /**
-//  *  On test si items[0[state] et order[0][state] existe est si ils ont les bonnes valeurs
-//  *  pour savoir si on déclare la vente comme validée..
-//  *  statut
-//  *  1 - attente
-//  *  2 - validée
-//  *  3 - échec
-//  *  4 - remboursée
-//  */
-// $array_statut = ['attente','validé','échec','remboursé'];
-
-//     if( isset($retour_hello['order']['items'][0]['state']) &&
-//         isset($retour_hello['order']['payments'][0]['state']) ){
-
-//             $statut = 1;
-
-//             echo $retour_hello['order']['items'][0]['state'];
-//             echo '<br>';
-//             echo $retour_hello['order']['payments'][0]['state'];
-
-
-//             if( $retour_hello['order']['items'][0]['state'] == 'Processed' &&
-//                 $retour_hello['order']['payments'][0]['state'] == 'Authorized' ){
-
-//                 $statut = 2;
-
-//             }
-//             if( $retour_hello['order']['items'][0]['state'] == 'Canceled' &&
-//                 $retour_hello['order']['payments'][0]['state'] == 'Refunded' ){
-
-//                 $statut = 4;
-                
-//             }
-
-//     }else{
-//         $statut = 3;
-//     }
-
-//         /**
-//          * Conversion du retour hello asso en array serializé pour mysql
-//          */
-//         $conn = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME );
-//         /**
-//          * Connection à la base ou error
-//          */
-//         if ($conn->connect_error) {
-//             echo die("Connection failed: " . $conn->connect_error);
-//         }
-//         /**
-//          * Test pour voir si $checkoutIntentId existe dans les tables wp_clients
-//          */
-//         $query = "SELECT * FROM {$table_name} WHERE `hello_id`={$checkoutIntentId}";
-//         $retour = $conn->query($query);
-
-//         /**
-//          * Si $checkoutIntentId existe on l'update avec le retour d'infos hello asso
-//          */
-//          if( $retour->num_rows ){
-
-//             /**
-//              * Serialization des metas et traitement pour les quotes avec mysqli_real_escape_string 
-//              */
-//             $retour_hello = serialize($retour_hello);
-//             $retour_hello = mysqli_real_escape_string($conn,$retour_hello);
-
-//             $date = new DateTime('now',new DateTimeZone('Europe/Paris'));
-//             $date = (clone $date)->format('Y-m-d H:i:s');
-
-//             $query_insert = "UPDATE {$table_name} SET `array`='{$retour_hello}' , `statut`='{$statut}' ,`date_update`='{$date}' WHERE `hello_id`={$checkoutIntentId} ";
-//             $conn->query($query_insert);
-
-
-//             echo '<img src="'.HELLOASSO_URL.'/templates/assets/checkout_ok.svg" />
-//                     <h3>Paiement '.$array_statut[$statut-1].'</h3>'.
-//                     '<button type="button" class="button button-primary">retour au site</button>';
-
-//             /*
-//             $query_test = "SELECT * FROM {$table_name} WHERE `hello_id`={$checkoutIntentId}";
-//             $retour_test = $conn->query($query_test);
-//             $row = $retour_test->fetch_array(MYSQLI_ASSOC);
-//             echo '<pre>';
-//             print_r(unserialize($row['array']));
-//             echo '<pre>';
-//             */
-
-//             $conn->close();
-
-//         }else{
-//             echo '<h3>id client inexistant !</h3>';
-//         }
-
-//     }else{
-//         echo '<h3>Paiement Refusé</h3>';
-//         // echo '<pre>';
-//         // print_r($retour_hello);
-//         // echo '</pre>';
-//     }
 
 
 ?>
