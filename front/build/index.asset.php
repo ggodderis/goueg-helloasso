37de128fe87b986ebf3f90ea31fd02cd8e63f43f
Gregory Godderis <g.godderis@spartoo.com>
2024-12-19 15:26:34 +0100

    Jeudi 19 décembre

--- front/build/index.asset.php
+++ front/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element'), 'version' => 'b7c8ff32f0af84291eb3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element'), 'version' => '0cc8cbcaa390f83d1ea0');
