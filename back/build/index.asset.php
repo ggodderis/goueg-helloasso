4ccce826f84391350955851b15ac1d07b03002ab
ggodderis <goueg@orange.fr>
2025-01-02 15:29:23 +0100

    --

--- back/build/index.asset.php
+++ back/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element'), 'version' => '007d3cdd6baf6100842e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element'), 'version' => '21f4ae44faac6470a4ca');
